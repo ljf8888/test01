cf24df049fe5b1e353ccc0c343bee766ad4bb079
Administrator <2813119976@qq.com>
2023-07-19 15:47:42 +0800

    first commit

---
+++ src/main/java/com/example/test/git.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+/**
+ * @Classname git
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2023/7/19 15:43
+ * @Created by 李炯飞
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
